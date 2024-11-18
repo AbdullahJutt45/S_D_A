94939acd4a39aba21c482ec6c456e88c4c664381
Abdullah <136979254+AbdullahJutt45@users.noreply.github.com>
2024-11-18 20:53:41 +0500

    Delete lab-mid sda/NotificationLogger.java

--- lab-mid sda/NotificationLogger.java
+++
@@ -1,6 +0,0 @@
-public class NotificationLogger implements TransportObserver {
-    @Override
-    public void update(String message) {
-        System.out.println("[NotificationLogger] Alert: " + message);
-    }
-}
